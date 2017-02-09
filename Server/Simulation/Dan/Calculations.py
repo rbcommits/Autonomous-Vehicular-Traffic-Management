3882693a666988cb57b9362b07dca22ae30860cf
Daniel Revie <daniel.revie1@gmail.com>
2017-02-08 22:40:08 -0500

    improvements in simulation as well as improving the collision management protocol

--- Server/Simulation/Dan/Calculations.py
+++ Server/Simulation/Dan/Calculations.py
@@ -1,8 +1,8 @@
 def collisionDetection(queueX, queueY):
     print "Entering Collistion Detection"
 
-#    print queueX.qsize()
-#    print queueY.qsize()
+    print queueX.qsize()
+    print queueY.qsize()
 
     if(not queueX.empty() and not queueY.empty()):
         carX = queueX.get()     # attain x car enterint intersection
@@ -12,10 +12,13 @@ def collisionDetection(queueX, queueY):
         if(abs(carX.positionX) < abs(carY.positionY)):
             # X is the first car
             print "x is the first car"
+            slowCar(carSlow=carY, carFull=carX, slowY=True)
 
         elif(abs(carY.positionY) < abs(carX.positionX)):
             # Y is the first car
             print "y is the first car"
+            slowCar(carSlow=carX, carFull=carY, slowY=False)
+
         else:
             # Both cars have the same position on their relative axis'
             print "they are the same!"
@@ -23,6 +26,38 @@ def collisionDetection(queueX, queueY):
         print "No cars in range of intersection"
 
 
-def slowCar(car):
+# Function to reduce velocity of a car
+def slowCar(carSlow, carFull, slowY):
+    print "Slow Car"
+    if(slowY):
+        print "Slow down the Y car"
+        while(True):
+            carSlow.velocityY = carSlow.velocityY - 0.1
+            if(collide(carX=carFull, carY=carSlow) is False):
+                break
 
-    print car.displayCar()
+    elif(not slowY):
+        print "Slow down the X car"
+        while(True):
+            carSlow.velocityX = carSlow.velocityX - 0.1
+            if(collide(carX=carSlow, carY=carFull) is False):
+                break
+
+
+# Function to detect a possible collision
+def collide(carX, carY):
+    print "Check for a test collision"
+    # Create two copies of cars to test
+    carTestX = carX
+    carTestY = carY
+    while(True):
+        carTestX.displayCar()
+        if((carTestX.positionX == carTestY.positionX) and (carTestX.positionY == carTestY.positionY)):
+            print "Test collision occured"
+            return True
+        if(abs(carTestX.positionX > 5)):
+            print "Test succesful regulation"
+            return False
+
+        carTestX.updatePosition(0.1)
+        carTestY.updatePosition(0.1)
--- Server/Simulation/Dan/Simulation.py
+++ Server/Simulation/Dan/Simulation.py
@@ -1,46 +1,79 @@
 import Calculations
+from random import randint
 from Car import Car
 from Intersection import Intersection
 
 
-# Initialize instance of intersection
-currIntersection = Intersection(100, 100, 10, 10)
+def randomCarGenerator():
+    direction = randint(1, 2)
+    if(direction == 1):
+        # Generate a vertically traveling car
+        velocityX = randint(1, 4)
+        car = Car(length=5, width=5, velocityX=velocityX, velocityY=0, startX=-20, startY=0)
+    elif(direction == 2):
+        # Generate a vertically traveling car
+        velocityY = randint(1, 4)
+        car = Car(length=5, width=5, velocityX=0, velocityY=velocityY, startX=0, startY=-20)
+
+    return car
 
 
-# Declare cars:
-# (self, length, width, velocityX, velocityY, startX, startY)
-car1 = Car(5, 5, 3, 0, -5, 0)
-car2 = Car(5, 5, 0, 3, 0, -5)
+def cleanList(carList):
+    for i in range(len(carList), 0):
+        if(carList[i].positionX > 5 or carList[i].positionY > 5):
+            del carList[i]
 
 
-# Initialize array to contain car
-carList = []                                            # initialize car array
-carList.append(car1)                                    # place car in array
-carList.append(car2)                                    # place car in array
+def simulation():
+    # Initialize instance of intersection
+    currIntersection = Intersection(100, 100, 10, 10)
+    # Declare cars:
+    # (self, length, width, velocityX, velocityY, startX, startY)
+    car1 = Car(5, 5, 3.1, 0, -20, 0)
+    car2 = Car(5, 5, 0, 3, 0, -20)
 
-
-for i in range(0, len(carList)):
-    carList[i].displayCar()
-
-
-elapsedTime = 0                                         # initialize time in seconds
-intervalTime = .5
-runSim = True                                           # bool to stop simulation
-
-# Begin simulation:
-while(runSim):
-
-    # Check to see if we should end simulation
-    if(elapsedTime > 10):
-        runSim = False                                  # if time condition is true runSim to false
-    elapsedTime += intervalTime                         # increment 100 mili second
-
-    # FIRST check if car is in the intersection
-    currIntersection.updateIntersectionQueues(carList, elapsedTime)
-    # SECOND alert Calculations file cars that are in intersection
-    Calculations.collisionDetection(currIntersection.queueX, currIntersection.queueY)
+    # Initialize array to contain car
+    carList = []                                            # initialize car array
+    carList.append(car1)                                    # place car in array
+    carList.append(car2)                                    # place car in array
 
     for i in range(0, len(carList)):
-        # THIRD update position
-        carList[i].updatePosition(intervalTime)
-        carList[i].displayPosition()
+        carList[i].displayCar()
+
+    elapsedTime = 0                                         # initialize time in seconds
+    intervalTime = 1
+    runSim = True                                           # bool to stop simulation
+
+    # Begin simulation:
+    while(runSim):
+
+        # Remove processed cars from the intersection lsit 
+        cleanList(carList)
+
+        # Check to see if we should end simulation
+        if(elapsedTime > 10):
+            runSim = False                                  # if time condition is true runSim to false
+        elapsedTime += intervalTime                         # increment 100 mili second
+
+        # Check if cars are in intersection range
+        currIntersection.updateIntersectionQueues(carList, elapsedTime)
+        # Check for possible collisions
+        Calculations.collisionDetection(currIntersection.queueX, currIntersection.queueY)
+
+        # Update the positions of the cars in the list
+        for i in range(0, len(carList)):
+            # THIRD update position
+            carList[i].updatePosition(intervalTime)
+            carList[i].displayPosition()
+
+        # Every seconds generate a new car
+        if(elapsedTime % 1 is 0):
+            # Generate a new car
+            newCar = randomCarGenerator()
+            carList.append(newCar)
+
+        # If a car has exited the intersection remove it from the array 
+    print len(carList)
+
+
+simulation()
