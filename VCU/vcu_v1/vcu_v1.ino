ced13561ef50955aa4b790ca00d00b28d11e71ca
Kevin Zheng <kz476@outlook.com>
2017-04-17 15:43:45 -0400

    i2c code rewrite (work in progress)

--- VCU/vcu_v1/vcu_v1.ino
+++ VCU/vcu_v1/vcu_v1.ino
@@ -5,6 +5,7 @@
    Hall Effect Sensor on pin A0
 */
 
+#include <Wire.h>
 #include <PID_v1.h>
 
 int vSen = A0;
@@ -15,8 +16,15 @@ bool vSenLastState = false;
 unsigned long vSenTimeout = 500000;
 int mPinFwdA = 9;
 int mPinFwdB = 10;
+int sComPinL = 5;
+int sComPinR = 6;
 unsigned long scrUpdateTimer = 0;
 unsigned long scrUpdateInt = 250;
+int vCom = 0;
+int sCom = 0;
+int vComPwmMax = 95;
+int sComPwmMax = 95;
+
 
 double mSpeedSet, mSpeedAct, mSpeedPwm;
 
@@ -24,19 +32,19 @@ double mSpeedSet, mSpeedAct, mSpeedPwm;
 PID mPID(&mSpeedAct, &mSpeedPwm, &mSpeedSet, 0.07, 0.25, 0.005, DIRECT);
 
 void setup() {
+  Wire.begin(0x0a);
+  Wire.onReceive(i2c_recv);
   Serial.begin(115200);
   pinMode(vSen, INPUT);
   pinMode(mPinFwdA, OUTPUT);
   pinMode(mPinFwdB, OUTPUT);
-
-  delay(1000);
   analogWrite(mPinFwdA, 127);
   mSpeedSet = 1500;
   mPID.SetMode(AUTOMATIC);
 }
 
 void loop() {
-  vSenUpdate(); 
+  vSenUpdate();
 }
 
 void vSenUpdate() {
@@ -58,7 +66,31 @@ void vSenUpdate() {
 void pidUpdate() {
   mSpeedAct = vSenSpeed;
   mPID.Compute();
-  analogWrite(mPinFwdA, mSpeedPwm);
-  analogWrite(mPinFwdB, mSpeedPwm);
+  mDriveSet();
 }
 
+void mDriveSet(){
+  if(mSpeedPwm <= vComPwmMax){
+    analogWrite(mPinFwdA, mSpeedPwm);
+  } else {
+    analogWrite(mPinFwdA, vComPwmMax);
+  }
+  if(mSpeedPwm <= vComPwmMax){
+    analogWrite(mPinFwdB, mSpeedPwm);
+  } else {
+    analogWrite(mPinFwdB, vComPwmMax);
+  }
+
+
+
+}
+
+void i2c_recv(int length){
+  while(Wire.available() > 0){
+    int c = Wire.read();
+    if(c == 'c'){
+      vCom = Wire.read();
+      sCom = Wire.read();
+    }
+  }
+}
